99c32c8bbdac21a9caffa095beb91507fb44ceae
howard <howardfk@gmail.com>
2015-11-14 14:00:53 -0600

    Swapped the Update movment code from the Character > Player

--- Character.rb
+++ Character.rb
@@ -55,42 +55,4 @@ class Character
     @image.draw_rot(self.x_pos, self.y_pos, 1, self.angle, 0.5, 0.7)
   end
 
-  # Chnge name if needed 
-  # Passe values to the managment class to update on window
-  def update(mouse_x, mouse_y)
-    w_down = Gosu::button_down? Gosu::KbW
-    a_down = Gosu::button_down? Gosu::KbA
-    s_down = Gosu::button_down? Gosu::KbS
-    d_down = Gosu::button_down? Gosu::KbD
-    left_shift_down = Gosu::button_down? Gosu::KbLeftShift
-
-    x_vel=0
-    y_vel=0
-
-    if w_down && s_down
-    elsif w_down
-      y_vel=-1
-    elsif s_down
-      y_vel=1
-    end
-
-    if a_down && d_down
-    elsif a_down
-      x_vel=-1
-    elsif d_down
-      x_vel=1
-    end
-
-    if left_shift_down
-      self.is_running = true
-    else
-      self.is_running = false
-    end
-
-    self.move(x_vel, y_vel)
-
-    # Update direction of player after setting player's x,y 
-    self.face_to(mouse_x, mouse_y)     
-  end
-
 end
--- Player.rb
+++ Player.rb
@@ -4,4 +4,43 @@ class Player < Character
   def initialize(image, speed=4, speed_mod=2, x_pos=100, y_pos=100, angle=0) 
     super(image, speed, speed_mod, x_pos, y_pos, angle)
   end
+
+  # Change name if needed 
+  # Passe values to the managment class to update on window
+  def update(mouse_x, mouse_y)
+    w_down = Gosu::button_down? Gosu::KbW
+    a_down = Gosu::button_down? Gosu::KbA
+    s_down = Gosu::button_down? Gosu::KbS
+    d_down = Gosu::button_down? Gosu::KbD
+    left_shift_down = Gosu::button_down? Gosu::KbLeftShift
+
+    x_vel=0
+    y_vel=0
+
+    if w_down && s_down
+    elsif w_down
+      y_vel=-1
+    elsif s_down
+      y_vel=1
+    end
+
+    if a_down && d_down
+    elsif a_down
+      x_vel=-1
+    elsif d_down
+      x_vel=1
+    end
+
+    if left_shift_down
+      self.is_running = true
+    else
+      self.is_running = false
+    end
+
+    self.move(x_vel, y_vel)
+
+    # Update direction of player after setting player's x,y 
+    self.face_to(mouse_x, mouse_y)     
+  end
+ 
 end
