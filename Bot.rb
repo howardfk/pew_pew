14a8c54760d239a9baf89b649015b366652a957a
howard <howardfk@gmail.com>
2015-11-17 18:30:41 -0600

    Added bare bone Bot to main

--- Bot.rb
+++ Bot.rb
@@ -1,4 +1,12 @@
 require_relative 'Character'
 
 class Bot < Character
+  def initialize(image, speed=4, speed_mod=2, x_pos=500, y_pos=500, angle=0) 
+    super(image, speed, speed_mod, x_pos, y_pos, angle)
+  end
+
+  def update
+    #will nee to ad some kind of AI but for now bot will do nothing
+  end
+
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require 'gosu'
 
+require_relative 'Bot'
 require_relative 'Player'
 #require_relative 'wall'
 
@@ -16,50 +17,19 @@ class GameWindow < Gosu::Window
 
     # Must pass in image when creating player
     @player = Player.new(Gosu::Image.new(self, "assets/player.png", false))
+    @bot = Bot.new(Gosu::Image.new(self, "assets/player.png", false))
     #@wall = Wall.new(10,50)
   end
 
   def update
     # Player info that is upated, movment requires the mouse_y from Gosu::window
     @player.update(self.mouse_x, self.mouse_y)
-    # Fallowing 34 lines moved to Charicter#Update
-    #w_down = Gosu::button_down? Gosu::KbW
-    #a_down = Gosu::button_down? Gosu::KbA
-    #s_down = Gosu::button_down? Gosu::KbS
-    #d_down = Gosu::button_down? Gosu::KbD
-    #left_shift_down = Gosu::button_down? Gosu::KbLeftShift
-
-    #x_vel=0
-    #y_vel=0
-
-    #if w_down && s_down
-    #elsif w_down
-      #y_vel=-1
-    #elsif s_down
-      #y_vel=1
-    #end
-
-    #if a_down && d_down
-    #elsif a_down
-      #x_vel=-1
-    #elsif d_down
-      #x_vel=1
-    #end
-
-    #if left_shift_down
-      #@player.is_running=true
-    #else
-      #@player.is_running=false
-    #end
-
-    #@player.move(x_vel, y_vel)
-
-    ## Update direction of player after setting player's x,y 
-    #@player.point_to(self.mouse_x, self.mouse_y)     
+    @bot.update
   end
 
   def draw
     @player.draw
+    @bot.draw
     #@wall.draw
   end
 
