a4b2c6233a00517ccf0283e2e8d010f3f9fc1a5f
howard <howardfk@gmail.com>
2015-12-23 02:34:09 -0600

    Fixed the insert bug

--- impact_tree.rb
+++ impact_tree.rb
@@ -5,8 +5,8 @@
 # data_list are all the objects your intersted in tracking stored in a node
 
 class ImpactTree
-  @@max_data = 1000
-  @@max_level = 20000
+  @@max_data = 4
+  @@max_level = 3
   attr_accessor :level, :node_bounds, :nodes, :data_list
   # node_bounds is a Shape that has positoin, and dimentions.
   # More info in the shape class
@@ -26,59 +26,79 @@ class ImpactTree
   # Splits a node into children subnodes with new sub-bounds.
   # Assume all bounds are retangles in this model 
   def branch
-    sub_hight= self.node_bounds.width/2
-    sub_width = self.node_bounds.hight/2
+    sub_hight= self.node_bounds.width/2.0
+    sub_width = self.node_bounds.hight/2.0
     x = self.node_bounds.x
     y = self.node_bounds.y
 
     self.nodes = Array.new
-    nodes << ImpactTree.new(self.level += 1, Rectangle.new(x + sub_width, y + sub_hight, sub_width, sub_hight))
-    nodes << ImpactTree.new(self.level += 1, Rectangle.new(x - sub_width, y + sub_hight, sub_width, sub_hight))
-    nodes << ImpactTree.new(self.level += 1, Rectangle.new(x - sub_width, y - sub_hight, sub_width, sub_hight))
-    nodes << ImpactTree.new(self.level += 1, Rectangle.new(x + sub_width, y - sub_hight, sub_width, sub_hight))
+    level = self.level + 1
+    self.nodes << ImpactTree.new(Rectangle.new(x + sub_width/2, y + sub_hight/2, sub_width, sub_hight), level)
+    self.nodes << ImpactTree.new(Rectangle.new(x - sub_width/2, y + sub_hight/2, sub_width, sub_hight), level)
+    self.nodes << ImpactTree.new(Rectangle.new(x - sub_width/2, y - sub_hight/2, sub_width, sub_hight), level)
+    self.nodes << ImpactTree.new(Rectangle.new(x + sub_width/2, y - sub_hight/2, sub_width, sub_hight), level)
   end
 
   # obj_bounds is a Shape class object
-  # getQaud retuns index 1..4 represent each subquad
+  # getQuad retuns index 0..3 represent each subquad
   def getQuad(obj_bounds)
+    if (obj_bounds == nil) 
+      puts "obj_boudns not defined at get quad"
+    end
     index = -1
-    
     #Boolen values indicate if item is complety contained in half boundrys
     #remeber self.nodes[0].lower == self.nodes[3].upper
-    top_half    = (obj_bounds.upper > self.nodes[0].upper) && (obj_bounds.lower < self.nodes[0].lower) ? true : false
-    bottom_half = (obj_bounds.upper > self.nodes[0].lower) && (obj_bounds.lower < self.nodes[4].lower) ? true : false
-    left_half   = (obj_bounds.left  > self.nodes[1].left ) && (obj_bounds.right < self.nodes[1].right) ? true : false
-    right_half  = (obj_bounds.right < self.nodes[0].right) && (obj_bounds.left  > self.nodes[1].left ) ? true : false
+    top_half    = ((obj_bounds.upper > self.nodes[0].node_bounds.upper) && (obj_bounds.lower < self.nodes[0].node_bounds.lower)) ? true : false
+    top_half    = ((obj_bounds.upper > self.nodes[0].node_bounds.upper) && (obj_bounds.lower < self.nodes[0].node_bounds.lower)) ? true : false
+    bottom_half = ((obj_bounds.upper > self.nodes[0].node_bounds.lower) && (obj_bounds.lower < self.nodes[3].node_bounds.lower)) ? true : false
+    left_half   = ((obj_bounds.left  > self.nodes[1].node_bounds.left ) && (obj_bounds.right < self.nodes[1].node_bounds.right)) ? true : false
+    right_half  = ((obj_bounds.right < self.nodes[0].node_bounds.right) && (obj_bounds.left  > self.nodes[1].node_bounds.left )) ? true : false
 
-    index = top_half    && right_half ? 0 : index
-    index = top_half    && left_half  ? 1 : index
-    index = bottom_half && left_half  ? 2 : index
-    index = top_half    && right_half ? 3 : index
+    index = (top_half    && right_half) ? 0 : index
+    index = (top_half    && left_half ) ? 1 : index
+    index = (bottom_half && left_half ) ? 2 : index
+    index = (top_half    && right_half) ? 3 : index
 
     # Need to consider outlyer case if object moves past the edge of map where:
     #   obj_bounds.upper && obj_bounds.lower < 0
     # This probably will need to be addressed in the managment class
   end
 
-  # Incerting shapes for collition detection must be from the shape class
+  # obj_bounds passed for collition detection must be from the shape class
   def insert(obj_bounds)
-    self.nodes[0] != nil ? 
-      (index = getQuad(obj_bounds) 
-       index != -1 ? nodes[index].insert(obj_bounds) : nil) : nil
+    if (obj_bounds == nil) 
+      puts "obj_boudns not defined @@@@@ insert"
+    end
+    if self.nodes.empty? == false
+      index = getQuad(obj_bounds)
+      if index != -1
+        self.nodes[index].insert(obj_bounds)
+      end
+    end
 
     self.data_list << obj_bounds
 
-    (data_list.length > @@max_data) && (level < @@max_level) ? 
-       (nodes[0] == nil ? branch : self.data_list.each{|obj_bounds| index = getQuad(obj_bounds)
-                                                  index != -1 ? nodes[index] << obj_bounds : nil}) : nil
+    if ((self.data_list.length > @@max_data) && (level < @@max_level))
+      if self.nodes[0] == nil
+        self.branch
+      end 
+
+      self.data_list.each{|thing| 
+        index = getQuad(thing)
+        #index = 1
+        if index != -1
+          self.nodes[index].data_list << obj_bounds 
+        end}
+    end
   end
 
   # I feel like there is a faster way to do the retrive by 
   #   noting the position of each object in the tree.... 
   def retrive(obj_bounds, return_data = Array.new)
     index = getQuad(obj_bounds)
-    (index != -1) && (self.nodes[0] != nil) ? retrive(obj_bounds, return_data) : nil
+    if (index != -1) && (self.nodes[0] != nil)
+      self.nodes[index].retrive(obj_bounds, return_data)
+    end
     self.data_list.each{|obj_bounds| return_data << obj_bounds}
   end
-
 end
