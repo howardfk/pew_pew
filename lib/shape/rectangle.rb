695b56135f4cd92bdcad4423d2ecd160f880c3bf
howard <howardfk@gmail.com>
2015-12-30 17:28:49 -0600

    corrected spelling of method height

--- lib/shape/rectangle.rb
+++ lib/shape/rectangle.rb
@@ -4,16 +4,16 @@ require_relative 'Polygon'
 
 class Rectangle < Polygon
   attr_reader :perimeter, :area, :upper, :lower, :left, :right
-  attr_accessor :hight, :width
+  attr_accessor :height, :width
 
-  def initialize(x, y, width, hight)
+  def initialize(x, y, width, height)
     super(x, y, 4)
-    @hight = hight
+    @height = height
     @width = width
-    @area  = hight*width
-    @perimeter = 2*hight*width
-    @upper = y - hight/2
-    @lower = y + hight/2
+    @area  = height*width
+    @perimeter = 2*height*width
+    @upper = y - height/2
+    @lower = y + height/2
     @left  = x - width/2 
     @right = x + width/2
   end
