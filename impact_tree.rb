22fa70ec317b9bcc6115f13dcf17e2c8b64f54a0
howard <howardfk@gmail.com>
2016-01-02 02:36:44 -0600

    Identifed location of duplicate error, added debugging methods

--- impact_tree.rb
+++ impact_tree.rb
@@ -5,7 +5,7 @@
 # data_list are all the objects your intersted in tracking stored in a node
 
 class ImpactTree
-  @@max_data = 2
+  @@max_data = 5
   @@max_level = 4
   attr_accessor :level, :node_bounds, :nodes, :data_list
   # node_bounds is a Shape that has positoin, and dimentions.
@@ -45,7 +45,6 @@ class ImpactTree
   # re write get quad to return "node tree structure" and use bounds and data to do waht we need
   def getQuad(obj_bounds)
     index = -1
-    puts self.inspect
     top_half    = ((obj_bounds.upper > self.nodes[0].node_bounds.upper) && (obj_bounds.lower < self.nodes[0].node_bounds.lower)) ? true : false
     #top_half    = ((obj_bounds.upper > self.nodes[0].node_bounds.upper) && (obj_bounds.lower < self.nodes[0].node_bounds.lower)) ? true : false
     right_half  = ((obj_bounds.right < self.nodes[0].node_bounds.right) && (obj_bounds.left  > self.nodes[0].node_bounds.left )) ? true : false
@@ -70,40 +69,44 @@ class ImpactTree
     # rewrite the  next 6 lines to use a returned node instead of index for get quad
     unless self.nodes.empty?
       index = self.getQuad(obj_bounds)
-      if index != -1
+      unless index == -1
         self.nodes[index].insert(obj_bounds)
       end
     end
 
     self.data_list << obj_bounds
 
-    if ((self.data_list.length > @@max_data) && (level < @@max_level))
-      if self.nodes.empty? == true
+    if ((self.data_list.length > @@max_data) && (self.level < @@max_level))
+      if self.nodes.empty?
         self.branch
       end 
 
       self.data_list.each{|thing| 
         index = self.getQuad(thing)
-        if index != -1
+        unless index == -1
           self.nodes[index].data_list << thing
+          # ERROR IS BELOW
           self.data_list.delete(thing)
-        end}
+        end
+        }
     end
   end
 
   # I feel like there is a faster way to do the retrieve by 
-  #   noting the position of each object in the tree.... 
-  def retrieve(obj_bounds, return_data = Array.new)
-    #look into if the getQaud is causing an error for the root case
-    #unless self.node.empty?
-    if (self.nodes[0] != nil)
+  # noting the position of each object in the tree.... 
+  def retrieve(obj_bounds) #, return_data = Array.new)
+    unless self.nodes.empty?
       index = self.getQuad(obj_bounds)
-      if index != -1 
-        self.nodes[index].retrieve(obj_bounds, return_data)
+      unless index == -1 
+        self.nodes[index].retrieve(obj_bounds)
       end
     end
+    
     unless self.data_list.empty?
-      self.data_list.each{|obj_bounds| return_data << obj_bounds}
+      #if defined? return_data == nil
+        return_data = Array.new
+      #end
+      self.data_list.each{|things| return_data << things}
     end
   end
 
@@ -115,5 +118,31 @@ class ImpactTree
       self.nodes[3].get_node_bounds(return_bounds)
     end
     return_bounds << self.node_bounds 
+  end
+  
+  def get_nodes(node_list = Array.new)
+    unless self.nodes.empty?
+      self.nodes[0].get_nodes(node_list)
+      self.nodes[1].get_nodes(node_list)
+      self.nodes[2].get_nodes(node_list)
+      self.nodes[3].get_nodes(node_list)
+    end
+    node_list << self
+  end
+
+  def puts_nodes
+    node_list = self.get_nodes
+    i=0
+    node_list.each{|thing| 
+      if i==0
+        puts "THE START: NODE INFO"
+      end
+      i+=1
+      puts "
+            Lvl: #{thing.level}
+            items: #{thing.data_list.length}
+            detales: #{thing.data_list.inspect}
+            location: #{thing.node_bounds.x} #{thing.node_bounds.y}
+            "}
   end
 end
